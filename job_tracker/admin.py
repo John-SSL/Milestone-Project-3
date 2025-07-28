461f76719d6a333bd13b3d426ceac17b53c1d920
John-SSL <jsscadden@gmail.com>
2025-07-28 13:54:29 +0100

    Create Job Type and Completed Job models

--- job_tracker/admin.py
+++ job_tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import JobType, CompletedJob
 
 # Register your models here.
+
+admin.site.register(JobType)
+admin.site.register(CompletedJob)
--- job_tracker/models.py
+++ job_tracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class JobType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    credits = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} ({self.credits} credits)"
+
+
+class CompletedJob(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    job_type = models.ForeignKey(JobType, on_delete=models.CASCADE)
+    completed_on = models.DateField()
