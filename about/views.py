2e5943235697858f8607ef043ab1ece9030275fc
John-SSL <jsscadden@gmail.com>
2025-07-29 12:54:13 +0100

    Set up About view and urls

--- about/views.py
+++ about/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import About
 
 # Create your views here.
+
+def about_us(request):
+    return HttpResponse("This is the About Us page")
--- ctrack/urls.py
+++ ctrack/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from job_tracker import urls as job_tracker_urls
 
 urlpatterns = [
+    path('about/', include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include(job_tracker_urls), name="job_tracker_urls")
