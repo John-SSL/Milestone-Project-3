2cb627a70423f2cef5280db0403e5da12d64e6f7
John-SSL <jsscadden@gmail.com>
2025-07-30 18:52:13 +0100

    Add navbar links

--- about/views.py
+++ about/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import About
 
 # Create your views here.
 
 def about_us(request):
-    return HttpResponse("This is the About Us/Home page")
+    return render(request, "about/about.html")
