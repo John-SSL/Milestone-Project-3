33cf4ae97bdc1f98aa7298b02a31aaa015297969
John-SSL <jsscadden@gmail.com>
2025-08-02 13:37:40 +0100

    Create absence model

--- job_tracker/admin.py
+++ job_tracker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import JobType, CompletedJob
+from .models import JobType, CompletedJob, Absence
 
 # Register your models here.
 
 admin.site.register(JobType)
 admin.site.register(CompletedJob)
+admin.site.register(Absence)
--- job_tracker/models.py
+++ job_tracker/models.py
@@ -15,3 +15,12 @@ class CompletedJob(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job_type = models.ForeignKey(JobType, on_delete=models.CASCADE)
     completed_on = models.DateField()
+
+
+class Absence(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    duration = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user} absent for {self.duration} hours on {self.date}"
